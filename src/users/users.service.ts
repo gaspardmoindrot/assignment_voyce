848bea4536348f27accb47d13ec705e53075c88c
Gaspard Moindrot <gaspardmoindrot@Air-de-Gaspard.attlocal.net>
2022-11-13 22:42:31 -0800

    🔧 Fix Users Service and id of findOne

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,9 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUsersDto } from './dto/createUsers.dto';
+import Users from './users.entity';
 import { UpdateUsersDto } from './dto/updateUsers.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import Users from './users.entity';
 
 @Injectable()
 export default class UsersService {
@@ -16,13 +16,13 @@ export default class UsersService {
     return this.usersRepository.find();
   }
 
-  /*async getUsersById(id: number) {
-    const users = await this.usersRepository.findOne(id);
+  async getUsersById(id: number) {
+    const users = await this.usersRepository.findOne({where: {id: id}});
     if (users) {
       return users;
     }
-    throw new HttpException('Users not found', HttpStatus.NOT_FOUND);
-  }*/
+    throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+  }
 
   async createUsers(users: CreateUsersDto) {
     const newUsers = await this.usersRepository.create(users);
@@ -30,19 +30,19 @@ export default class UsersService {
     return newUsers;
   }
 
-  /*async updateUsers(id: number, users: UpdateUsersDto) {
+  async updateUsers(id: number, users: UpdateUsersDto) {
     await this.usersRepository.update(id, users);
-    const updatedUsers = await this.usersRepository.findOne(id);
+    const updatedUsers = await this.usersRepository.findOne({where: {id: id}});
     if (updatedUsers) {
       return updatedUsers
     }
-    throw new HttpException('Users not found', HttpStatus.NOT_FOUND);
-  }*/
+    throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+  }
 
   async deleteUsers(id: number) {
     const deleteResponse = await this.usersRepository.delete(id);
     if (!deleteResponse.affected) {
-      throw new HttpException('Users not found', HttpStatus.NOT_FOUND);
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
   }
 }
